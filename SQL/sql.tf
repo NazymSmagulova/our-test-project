ad110b594efc53f910859aca8e03b1f552387dfd
EC2 Default User <ec2-user@ip-172-31-16-150.eu-west-2.compute.internal>
2022-03-26 19:04:25 +0000

    adsssfd

--- SQL/sql.tf
+++ SQL/sql.tf
@@ -24,43 +24,43 @@ data "terraform_remote_state" "main" {
   }
 }
 
-#output "vnet_info" {
-# value = data.terraform_remote_state.main.outputs.*
-#}
-
-
-
-resource "azurerm_mysql_server" "terraform" {
-  name                = "terraform-mysqlserver"
-  location            = "westus"
-  resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
-
-  administrator_login          = "mysqladmin"  
-  administrator_login_password = "H@Sh1CoR3!"
-
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                = true
-  backup_retention_days            = 7
-  geo_redundant_backup_enabled     = true
-  public_network_access_enabled    = false
-  ssl_enforcement_enabled          = true
-  ssl_minimal_tls_version_enforced = "TLS1_2"
+output "full_info" {
+ value = data.terraform_remote_state.main.outputs.*
 }
 
-resource "azurerm_mysql_database" "wordpress" {
-  name                = "wordpressdb"
-  resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
-  server_name         = data.terraform_remote_state.main.outputs.*
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
 
-output "fqdn" {
-  value = azurerm_mysql_server.terraform.fqdn
-}
+
+// resource "azurerm_mysql_server" "terraform" {
+//   name                = "terraform-mysqlserver"
+//   location            = "westus"
+//   resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
+
+//   administrator_login          = "mysqladmin"  
+//   administrator_login_password = "H@Sh1CoR3!"
+
+//   sku_name   = "B_Gen5_2"
+//   storage_mb = 5120
+//   version    = "5.7"
+
+//   auto_grow_enabled                = true
+//   backup_retention_days            = 7
+//   geo_redundant_backup_enabled     = true
+//   public_network_access_enabled    = false
+//   ssl_enforcement_enabled          = true
+//   ssl_minimal_tls_version_enforced = "TLS1_2"
+// }
+
+// resource "azurerm_mysql_database" "wordpress" {
+//   name                = "wordpressdb"
+//   resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
+//   server_name         = data.terraform_remote_state.main.outputs.*
+//   charset             = "utf8"
+//   collation           = "utf8_unicode_ci"
+// }
+
+// output "fqdn" {
+//   value = azurerm_mysql_server.terraform.fqdn
+// }
 
 
 
