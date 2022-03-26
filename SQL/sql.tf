b89447b3ab87653d35552d25ee6245e210c3b3ec
EC2 Default User <ec2-user@ip-172-31-16-150.eu-west-2.compute.internal>
2022-03-26 19:21:31 +0000

    adsssfdffgggg

--- SQL/sql.tf
+++ SQL/sql.tf
@@ -47,7 +47,6 @@ resource "azurerm_mysql_server" "terraform" {
 
   auto_grow_enabled                = true
   backup_retention_days            = 7
-  geo_redundant_backup_enabled     = true
   public_network_access_enabled    = false
   ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
