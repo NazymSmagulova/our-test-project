b3d0c0f8022d4a05b90a79ba8c6ab94f3acc2881
EC2 Default User <ec2-user@ip-172-31-16-150.eu-west-2.compute.internal>
2022-03-26 19:08:12 +0000

    adsssfdf

--- SQL/sql.tf
+++ SQL/sql.tf
@@ -28,27 +28,30 @@ output "full_info" {
  value = data.terraform_remote_state.main.outputs.*
 }
 
+output "id" {
+  value = azurerm_mysql_server.terraform.id
+}
 
 
-// resource "azurerm_mysql_server" "terraform" {
-//   name                = "terraform-mysqlserver"
-//   location            = "westus"
-//   resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
+resource "azurerm_mysql_server" "terraform" {
+  name                = "terraform-mysqlserver"
+  location            = "westus"
+  resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
 
-//   administrator_login          = "mysqladmin"  
-//   administrator_login_password = "H@Sh1CoR3!"
+  administrator_login          = "mysqladmin"  
+  administrator_login_password = "H@Sh1CoR3!"
 
-//   sku_name   = "B_Gen5_2"
-//   storage_mb = 5120
-//   version    = "5.7"
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
 
-//   auto_grow_enabled                = true
-//   backup_retention_days            = 7
-//   geo_redundant_backup_enabled     = true
-//   public_network_access_enabled    = false
-//   ssl_enforcement_enabled          = true
-//   ssl_minimal_tls_version_enforced = "TLS1_2"
-// }
+  auto_grow_enabled                = true
+  backup_retention_days            = 7
+  geo_redundant_backup_enabled     = true
+  public_network_access_enabled    = false
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
 
 // resource "azurerm_mysql_database" "wordpress" {
 //   name                = "wordpressdb"
