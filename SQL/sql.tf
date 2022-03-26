c33f9cd8d001cb16547466cc6d7febef9d0909b8
EC2 Default User <ec2-user@ip-172-31-16-150.eu-west-2.compute.internal>
2022-03-26 18:44:54 +0000

    addvf

--- SQL/sql.tf
+++ SQL/sql.tf
@@ -53,7 +53,7 @@ resource "azurerm_mysql_server" "terraform" {
 resource "azurerm_mysql_database" "wordpress" {
   name                = "wordpressdb"
   resource_group_name = data.terraform_remote_state.main.outputs.resource_group_name
-  server_name         = data.terraform_remote_state.main.outputs.id
+  server_name         = data.terraform_remote_state.main.outputs.*
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
